6f83b23ec7607a3a7eaf53d349ceee6849a06be9
Magenta Nyashka <lopatin.lenja@gmail.com>
2024-10-25 20:13:23 +0300

    Add project files.

---
+++ WpfApp2/Helper/FindRole.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WpfApp2.Model;
+using WpfApp2.ViewModel;
+namespace WpfApp2.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
---
+++ WpfApp2/ViewModel/RoleViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Model;
+
+namespace WpfApp2.ViewModel
+{
+    public class RoleViewModel
+    {
+        public ObservableCollection<Role> ListRole { get; set; } = new
+        ObservableCollection<Role>();
+        public RoleViewModel()
+        {
+            this.ListRole.Add(new Role
+            {
+                Id = 1,
+                NameRole = "Директор"
+            });
+            this.ListRole.Add(new Role
+            {
+                Id = 2,
+                NameRole = "Бухгалтер"
+            });
+            this.ListRole.Add(new Role
+            {
+                Id = 3,
+                NameRole = "Менеджер"
+            });
+        }
+    }
+}
